1b6c32ff45560c7ed1477a9e4a20d9466897f4c9
Pierre Roux <pierre.roux@onera.fr>
2025-10-20 10:50:56 +0200

    Remove all but micromega plugin

--- test-suite/output/bug_20242.v
+++
@@ -1,5 +0,0 @@
-Polymorphic Record foo@{s;u|} (x : Type@{s;u}) := {}.
-Inductive sEmpty : SProp := .
-Module Type A. Axiom A : Type. Axiom B : foo A. End A.
-Unset Universe Checking.
-Module B <: A. Axiom A : SProp. Axiom B : foo A. Fail End B.
